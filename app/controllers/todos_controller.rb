9b00020ae1717057efdec6cb812ca24e3a4899b5
Veena N <37401941+veenaguruprasad@users.noreply.github.com>
2021-10-04 20:36:01 +0530

    Formatted

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,25 +1,29 @@
 class TodosController < ApplicationController
-	skip_before_action :verify_authenticity_token
-	def index
-		render plain: Todo.to_displayable_list.join("\n")
-	end
-	def show
-		id = params[:id]
-		render plain: Todo.find(id).to_displayable_string
-	end
-	def update
-		id = params[:id]
-		completed = params[:completed]
-		todo = Todo.find(id)
-		todo.completed = completed
-		todo.save!
-		text = "Update todo completed status to #{completed}"
-		render plain: text
-	end
-	def create
-		text = params[:todo_text]
-		date = DateTime.parse(params[:due_date])
-		todo = Todo.create!(todo_text: text, due_date: date, completed: false)
-		render plain: "Hey, Your new todo is created with the id #{todo.id}!"
-	end
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render plain: Todo.to_displayable_list.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    render plain: Todo.find(id).to_displayable_string
+  end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    text = "Update todo completed status to #{completed}"
+    render plain: text
+  end
+
+  def create
+    text = params[:todo_text]
+    date = DateTime.parse(params[:due_date])
+    todo = Todo.create!(todo_text: text, due_date: date, completed: false)
+    render plain: "Hey, Your new todo is created with the id #{todo.id}!"
+  end
 end
