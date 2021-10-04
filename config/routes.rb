420a7e1129e7c94b98a03cf1d7a2c717df058f0a
Veena N <37401941+veenaguruprasad@users.noreply.github.com>
2021-10-04 20:42:39 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-
-  get "todos", to: "todos#index"
-  post "todos/create", to: "todos#create"
-  get "todos/:id", to: "todos#show"
-  patch "todos/:id", to: "todos#update"
-  resources "users"
+  resources :todos
+  resources :users
   post "/users/login"
-  
 end
