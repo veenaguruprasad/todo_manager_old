d73e520fa4dcb793bb7cb16e739dd2f905d4b479
Veena N <37401941+veenaguruprasad@users.noreply.github.com>
2021-10-04 20:36:50 +0530

    Formatted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,28 @@
 class UsersController < ApplicationController
-	skip_before_action :verify_authenticity_token
-	def index
-		render plain: User.to_displayable_list.join("\n")
-	end
-	def show
-		id = params[:id]
-		render plain: User.find(id).to_displayable_string
-	end
-	def create
-		name = params[:name]
-		email = params[:email]
-		password = params[:password]
-		user = User.create!(name: name, email: email, password: password)
-		render plain: "Hey, New User is created with the id #{user.id}"
-	end
-	def login
-		email = params[:email]
-		password = params[:password]
-		user = User.find_by(email: email, password: password)
-		presence = user.present?
-		render plain: presence
-	end 
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render plain: User.to_displayable_list.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    render plain: User.find(id).to_displayable_string
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    user = User.create!(name: name, email: email, password: password)
+    render plain: "Hey, New User is created with the id #{user.id}"
+  end
+
+  def login
+    email = params[:email]
+    password = params[:password]
+    user = User.find_by(email: email, password: password)
+    presence = user.present?
+    render plain: presence
+  end
 end
